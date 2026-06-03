bd19f292029cdb796617e38185751db2e5eb491a
Lssuriol <luz.uriol@tecsup.edu.pe>
2026-06-03 15:13:59 -0500

    Configurar URL del backend para producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://sistema-delivery-backend-57uo.onrender.com/api/',
 });
-
-export default api;
